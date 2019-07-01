2eff7153f7b89c2f3d4286ba60b4467a4dd186e2
River Phillips <riverphillips1@gmail.com>
2019-07-01 16:18:59 +0100

    Allow retrieving individual or list of links
    Signed-off-by: River Phillips <riverphillips1@gmail.com>

--- src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -1,18 +1,31 @@
 package com.howtographql.scala.sangria
 
 import com.howtographql.scala.sangria.models.Link
-import sangria.schema.{Field, ObjectType, IntType}
-
-import sangria.schema._
 import sangria.macros.derive._
+import sangria.schema.{Field, IntType, ObjectType, _}
 
 object GraphQLSchema {
- implicit val LinkType = deriveObjectType[Unit, Link]()
+  implicit val LinkType = deriveObjectType[Unit, Link]()
+
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
 
   val QueryType = ObjectType(
     "Query",
     fields[Context, Unit](
-      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
+      Field(
+        "link",
+        OptionType(LinkType),
+        arguments = Id :: Nil,
+        resolve = c => c.ctx.dao.getLink(c.arg(Id))
+      ),
+      Field(
+        "links",
+        ListType(LinkType),
+        arguments = Ids :: Nil,
+        resolve = c => c.ctx.dao.getLinks(c.arg(Ids))
+      )
     )
   )
 
